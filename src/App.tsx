82df049b6a50a4f8304da0262226fbe10968b8ca
Jordan Tay <jordanjiunftw@gmail.com>
2025-10-12 11:56:37 +0100

    feat: init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { TestComponent } from './components/TestComponent';
+
+function App() {
+  return (
+    <div>
+      <h1>My Pixi App</h1>
+      <TestComponent />
+    </div>
+  );
+}
+
+export default App;
